10437ab1cc3476926465316953108f23a0a2429d
Ctomes <72369162+Ctomes@users.noreply.github.com>
2022-10-22 19:52:01 -0700

    first commit with outline

---
+++ restaurant-bill.c
@@ -0,0 +1,8 @@
+//CS2600 Quiz 3 question 2 outline.
+//Tomes, Christopher
+
+//will create a main that stores a data structure containing the default meal costs. 
+//main will take up to 2 args from command line.
+//if none is taken; display prices without tax and tip applies.
+//if 1 or more, a meal will be chosen from data structure and the tax/tip will be applied.
+//display cost.
